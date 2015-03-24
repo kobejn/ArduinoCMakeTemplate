d78fa0a061a516a7067b7807e907514788970a22
mirage335 <spamfreemirage335@gmail.com>
2015-03-24 19:39:08 +0000

    first commit

---
+++ src/Blink/Blink.ino
@@ -0,0 +1,34 @@
+//http://arduino.cc/en/tutorial/blink
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+
+#include <avr/io.h>
+#include "prescaler.h"
+
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+
+void setup() {
+	setClockPrescaler(0x1);  //<8MHz for reliable 3.3V operation. >1MHz recommended.
+	
+	Serial.begin(115200);  // Maybe a lot faster over native USB.
+	
+	// initialize the digital pin as an output.
+	pinMode(led, OUTPUT);   
+}
+
+void loop() {
+	digitalWrite(led, HIGH);
+	trueDelay(1000);
+	Serial.println("HIGH");
+	
+	digitalWrite(led, LOW);
+	trueDelay(1000);
+	Serial.println("LOW");
+}
+
